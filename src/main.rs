83cb773dc98a25e2c34f8fad015223040fd4e2d8
gold-coding <andyjim815@gmail.com>
2024-07-02 09:45:41 -0500

    update

--- src/main.rs
+++ src/main.rs
@@ -14,15 +14,3 @@ async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
-#[actix_web::main]  
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
-    })
-    .bind(("127.0.0.1",8080))?
-    .run()
-    .await
-}
